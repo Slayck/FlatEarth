ad8d50dda7a0e9c8c2611738e2dcdcf775b6908b
Slayck <slayckx@gmail.com>
2017-10-28 23:09:31 +0400

    Update earthCurv.py

--- earthCurv.py
+++ earthCurv.py
@@ -1,33 +1,47 @@
-#Code developed by Slayck 
-#@SlayckX
 #EarthCurvature Calculator
 
-from math import cos, pi, radians
+from math import cos, acos, pi, radians, degrees
+def calcDrop(paramDistance,Perimetre,earthRadius):
+    Angle = (paramDistance * 360)/Perimetre
+    adjacent = cos(radians(Angle)) * earthRadius
+    hiddenDrop = earthRadius - adjacent
+    hiddenHeight = earthRadius * ( ((adjacent + hiddenDrop)/adjacent) -1 )
+    
+    
+    print ("Earth Circumference is:",Perimetre)
+    print ("Earth Angle for a Distance of 1KM: ",AngleFor1K)
+    print ("Angle of the Object Distance: ",Angle)
+    print ("Distance Adjacent to the Object Angle: ",adjacent)
+    print ("Hidden Drop(in meter): ",hiddenDrop*1000)
+    print ("Hidden Objects Max Height from the Surface(in meter): ",hiddenHeight * 1000)
 
-earthRadius = 6371#km
-Perimetre = 2 * pi * earthRadius #Earth Circumference
-AngleFor1K = 360 / Perimetre
+def calcEyeDrop(paramDistance,Perimetre,earthRadius,eyeHeight):
+    Angle = (paramDistance * 360)/Perimetre
+    cosine_alpha1 = earthRadius/(earthRadius+(eyeHeight/1000))
+    alpha1 = degrees(acos(cosine_alpha1))
+    alpha2 = Angle - alpha1
+    hiddenHeight = (earthRadius / (cos(radians(alpha2)))) - earthRadius
+    
+    
+    print ("Earth Circumference is:",Perimetre)
+    print ("Earth Angle for a Distance of 1KM: ",AngleFor1K)
+    print ("Angle of the Object Distance: ",Angle)
+    print ("Eye/Lens Height in KM: ", eyeHeight/1000)
+    print ("Cosine of Alpha1 angle i: ", cosine_alpha1)
+    print ("Alpha1 Angle is: ",alpha1)
+    print ("Alpha2 Angle is: ",alpha2)
+    print ("Hidden Objects Max Height from the Surface(in meter): ",hiddenHeight * 1000)
 
-paramDistance = input("What is the Distance of the Object in KM ") #Distance Parameter
+print ("Welcome to the Slaycken Earth Curve Calc")
 
-Angle = (paramDistance * 360)/Perimetre
-adjacent = cos(radians(Angle)) * earthRadius
-hiddenDrop = earthRadius - adjacent
-hiddenHeight = earthRadius * ( ((adjacent + hiddenDrop)/adjacent) -1 )
+earthRad = 6371#km
+Per = 2 * pi * earthRad #Earth Circumference
+AngleFor1K = 360 / Per
 
-hiddenX = earthRadius * (1 - cos(Angle)) #EarthCurves on Google
+Distance = input("What is the Distance of the Object in KM ") #Distance Parameter
+Eye = input("What is the Height from the ground to the Observer Lens in meters ") #Distance Parameter
 
-print ("Earth Circumference is:",Perimetre)
-print ("Earth Angle for a Distance of 1KM: ",AngleFor1K)
-print ("Angle of the Object Distance: ",Angle)
-print ("Distance Adjacent to the Object Angle: ",adjacent)
-print ("Hidden Drop: ",hiddenDrop)
-print ("Hidden Objects Max Height from the Surface(in meter): ",hiddenHeight * 1000)
-
-
-
-
-#print ("Hidden Objects Max Height from the ur EyE Height: ",paramInvisibleH)
-#dropHeight = 
-#distanceOnCurve = 
-#paramEyeHeight = input("What is Eye Height in Meter") #Distance Parameter
+if (Eye>0):
+    calcEyeDrop(Distance,Per,earthRad,Eye)    
+else: 
+    calcDrop(Distance,Per,earthRad)
